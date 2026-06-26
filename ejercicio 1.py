f63893b704b7b738d02a91e2e65089138fca4312
David-3-star <jose.salinas@correounivalle.edu.co>
2026-06-26 02:55:29 +0000

    ejercicio 1

---
+++ ejercicio 1.py
@@ -0,0 +1,17 @@
+#Paso por referencia
+import numpy as np
+
+def encontrar_menor_referencia(arreglo): #pass-by-object-reference o pass-by-assigment
+    menor = arreglo[0]
+    for valor in arreglo:
+        if valor < menor:
+            menor = valor
+    return menor
+
+if _name_ == "_main_":
+    # Arreglo de prueba con datos ficticios
+    y_datos = np.array([2.1, 4.5, 5.2, 3.8, 1.2])
+    
+    resultado = encontrar_menor_referencia(y_datos)
+    print("=== PUNTO 1 ===")
+    print(f"El menor valor dentro del arreglo es: {resultado}")
